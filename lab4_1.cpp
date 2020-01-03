82232880dc8f55f6cdcccd74f6594d80ac80a470
Apithep Piyaphiphathanamongkol <kaoapithepp@Apitheps-MacBook-Pro.local>
2020-01-03 15:08:23 +0700

    first try

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+float formuIn(float x){
+    float y = 3*(pow(x,3))+(2*2.71828)+pow(2,(2*x+1))+pow((pow(x,2)+1),0.5);
+    return y;
+}
+
+int main(){
+    int a;
+    cout << "Enter x: ";
+    cin >> a;
+    cout << "Result y = " << formuIn(a) << endl;
+    return 0;
+}
