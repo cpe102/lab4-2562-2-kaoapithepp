1caf701fdaab7d1df35ad1f98101648c79b96001
Apithep Piyaphiphathanamongkol <kaoapithepp@Apitheps-MacBook-Pro.local>
2020-01-03 16:15:17 +0700

    first try

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+int findDivisor(int x){
+    int i=2;
+    while (i > 1){
+        if (x%i == 0){
+            return i;
+        } else {
+            i++;
+        }
+    }
+}
+
+int main(){
+
+    int yourNumber;
+    cout << "Enter Your Number: ";
+    cin >> yourNumber;
+    cout << "The lowest number that can divide yours: " << findDivisor(yourNumber) << endl;
+
+    return 0;
+}
